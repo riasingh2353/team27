f30ce86d92b5708fce749ae8e11b4843078db309
bentaussig <42872932+bentaussig@users.noreply.github.com>
2018-09-21 15:32:29 -0400

    Add files via upload

---
+++ Code/Lab 2/audio/lab2fftanalysis.m
@@ -0,0 +1,17 @@
+Fs = 9600;
+for k = 1:128
+    freqs(k) = k*Fs/256;
+end
+
+fftdata = [ %PASTE OUTPUT OF teamalphafft.ino HERE!!!
+]
+
+
+
+
+hold on
+for k = 1:length(fftdata(:,1))
+    plot(freqs, fftdata(k,:))
+end
+xlabel('frequency (Hz)')
+ylabel('magnitude (dB)')
