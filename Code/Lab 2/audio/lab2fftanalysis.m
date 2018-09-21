ee833576b5f8fd8e899d8fac038d1bb5dc8f90d1
bentaussig <42872932+bentaussig@users.noreply.github.com>
2018-09-21 15:37:28 -0400

    Update lab2fftanalysis.m

--- Code/Lab 2/audio/lab2fftanalysis.m
+++ Code/Lab 2/audio/lab2fftanalysis.m
@@ -1,6 +1,6 @@
 Fs = 9600;
 for k = 1:128
-    freqs(k) = k*Fs/256;
+    freqs(k) = k*Fs/256; %calculate values of frequency bins
 end
 
 fftdata = [ %PASTE OUTPUT OF teamalphafft.ino HERE!!!
@@ -11,7 +11,7 @@ fftdata = [ %PASTE OUTPUT OF teamalphafft.ino HERE!!!
 
 hold on
 for k = 1:length(fftdata(:,1))
-    plot(freqs, fftdata(k,:))
+    plot(freqs, fftdata(k,:)) 
 end
 xlabel('frequency (Hz)')
 ylabel('magnitude (dB)')
