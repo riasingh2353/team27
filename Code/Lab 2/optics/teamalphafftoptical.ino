1434cbda247983a15fd69764fe94a733d91f9846
bentaussig <42872932+bentaussig@users.noreply.github.com>
2018-09-25 18:21:19 -0400

    FFT script. Runs fast enough to detect optical signal
    The ADC runs ~eight times faster than default in this code. See detailed explanation here: http://www.gammon.com.au/adc . Otherwise, this code is essentially the same as the team alpha-based code used by the audio subteam.

---
+++ Code/Lab 2/optics/teamalphafftoptical.ino
@@ -0,0 +1,35 @@
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+#include <FFT.h> // include the library
+
+void setup() {
+  ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits 
+  ADCSRA |= bit (ADPS2);         // set ADC prescalar to be eight times faster than default
+  Serial.begin(115200); // use the serial port
+}
+int l = 0;
+void loop() {
+  while(1) {
+    cli();
+    for (int i = 0 ; i < 512 ; i += 2) {
+      fft_input[i] = analogRead(A0); // <-- NOTE THIS LINE
+      fft_input[i+1] = 0;
+    }
+    fft_window();
+    fft_reorder();
+    fft_run();
+    fft_mag_log();
+    sei();
+
+    for (int i = 0 ; i < FFT_N/2 ; i++) {
+      Serial.print(fft_log_out[i]);
+      Serial.print(' ');
+    }
+    Serial.println();
+    l = l + 1;
+    if (l == 20) {
+      delay(100000000);
+    }
+  }
+}
