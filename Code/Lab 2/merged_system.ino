9d6e16666af56054578c2644c8646cf6ed61ff31
bentaussig <42872932+bentaussig@users.noreply.github.com>
2018-10-05 13:35:52 -0400

    Create merged_system.ino

---
+++ Code/Lab 2/merged_system.ino
@@ -0,0 +1,66 @@
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+#include <FFT.h> // include the library
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits 
+  ADCSRA |= bit (ADPS2);         // set ADC prescalar to be eight times faster than default
+  Serial.begin(115200); // use the serial port
+}
+
+int l = 0;
+int count = 0;
+boolean start = 0;
+
+
+
+void loop() {
+  while(1) {
+    cli();
+    for (int i = 0 ; i < 512 ; i += 2) {
+      fft_input[i] = analogRead(A0); // <-- NOTE THIS LINE
+      fft_input[i+1] = 0;
+    }
+    fft_window();
+    fft_reorder();
+    fft_run();
+    fft_mag_log();
+    sei();
+      
+      if (start == 0) { 
+        if (fft_log_out[3] > 70){
+          l = l + 1;
+          digitalWrite(2, LOW);
+        }
+        else {
+          l = 0;
+          digitalWrite(2, LOW);
+        }
+        if (l >= 10) {
+          count = count + 1;
+          start = 1;
+          digitalWrite(2, HIGH);  //flip select bit
+          digitalWrite(3, HIGH);  //turn on indicator LED
+          Serial.println("660 HURTS !!!!!");
+        }
+      }
+      
+      if (start == 1) {
+        digitalWrite(2, HIGH);
+        if (fft_log_out[26] > 60 || fft_log_out[25] > 60 || fft_log_out[27] > 60){
+          Serial.println("6KHz !!!!!");
+          digitalWrite(4, HIGH); //turn on indicator LED
+        }
+        else {
+          digitalWrite(4, LOW); //turn off indicator LED
+        }
+      }
+  }
+}
