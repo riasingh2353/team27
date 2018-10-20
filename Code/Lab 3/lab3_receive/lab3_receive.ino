5080e6c70fda99414568eedefb97065bbb0695c4
Ria Singh <rs2353@cornell.edu>
2018-10-20 17:43:37 -0400

    updated receive code

--- Code/Lab 3/lab3_receive/lab3_receive.ino
+++ Code/Lab 3/lab3_receive/lab3_receive.ino
@@ -13,7 +13,11 @@ RF24 radio(9,10);
 const uint64_t pipes[2] = { 0x0000000048LL, 0x0000000049LL };
 byte zero[3] = {0, 0, 0};
 byte data_before [3];
+byte data[3];
 int data_array[24];
+int count_x = -1;
+int count_y = 0;
+bool first = true;
 
 // Intialize strings to print
 String x = "0";
@@ -26,10 +30,10 @@ String robot;
 
 
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(9600);
   printf_begin();
-  printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
+  //printf("\n\rBase Station Receive Code\n\r");
+  //printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
   //
   // Setup and configure rf radio
   //
@@ -62,81 +66,111 @@ void setup() {
   //
   // Dump the configuration of the rf unit for debugging
   //
-   radio.printDetails();
+  // radio.printDetails();
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned long data[3];
-  if ( radio.available() )
-    {
-      // Dump the payloads until we've gotten everything
-      byte data[3];
-      bool done = false;
-      while (!done)
+    if ( radio.available() )
       {
-        // Fetch the payload, and see if this was the last one.
-        radio.read(&data,sizeof(data));
-        if (data != zero){
-          printf("Reading data...\n\r");
-           copy(data, data_before, 3);
-          //printf(data);
+        // Dump the payloads until we've gotten everything
+        bool done = false;
+        while (!done)
+        {
+          // Fetch the payload, and see if this was the last one.
+          done=radio.read(&data,sizeof(data));
+          if (check_data()){
+             decipher();
+          }
+          copy(data, data_before, 3);
+          delay(20);
         }
-        if (data == zero && data_before != zero){
-          decipher();
-        }
-        // Spew it
-        printf("Got payload %lu...\n\r",data);
-        // Delay just a little bit to let the other unit
-        // make the transition to receiver
-        delay(20);
       }
-    }
-    
-//Final output to serial
-
-
-//  Serial.println(data_before[1]);
-//  for(int i=0; i<24;i++) {
-//    Serial.print(data_array[i]);
-//  }
-//  Serial.println("");
-// Serial.println(x+","+y+","+"north="+north+","+"east="+east+","+"south="+south+","+"west="+west+","+"robot="+robot);
-
 }
 
 void decipher(){
+  //Serial.println("Deciphering");
   for (int i = 0; i<3; i++){
     for (int j = 0; j < 8; j++){
       int x = 8*i + j;
       data_array[x] = int(bitRead(data_before[i], 7-j));
       //Serial.println(x);
-      //Serial.print(data_array[x]);
+     // Serial.print(data_array[x]);
      }
    }
+
+   count_x++;
+
+   if (count_x > 2) {
+    count_y++;
+    count_x = 0;
+   }
+   if (count_y > 3) {
+    count_y = 0;
+   }
    //Serial.println("");
-//   for(int i = 0; i<24; i++)
-//   {
-//      Serial.print(data_array[i]);
-//   }
-//   Serial.println("");
- 
- if (data_array[1]==1) {robot = "true";}
- else {robot = "false";}  
+   if (data_array[1]==1) {robot = "true";}
+   else {robot = "false";}  
+     
+   if (data_array[4]==1) {north = "true";}
+   else {north = "false";}
    
- if (data_array[4]==1) {north = "true";}
- else {north = "false";}
- 
- if (data_array[5]==1) {east = "true";}
- else {east = "false";}
- 
- if (data_array[6]==1) {south = "true";}  
- else {south = "false";}
- 
- if (data_array[7]==1) {west = "true";}
- else {west = "false";}
+   if (data_array[5]==1) {east = "true";}
+   else {east = "false";}
+   
+   if (data_array[6]==1) {south = "true";}  
+   else {south = "false";}
+   
+   if (data_array[7]==1) {west = "true";}
+   else {west = "false";}
+
+  x = String(count_x);
+  y = String(count_y);
+
+   Serial.println(x+","+y+","+"north="+north+","+"east="+east+","+"south="+south+","+"west="+west+","+"robot="+robot);
  
 }
 
 void copy(byte* src, byte* dst, int len) {
     memcpy(dst, src, sizeof(src[0])*len);
 }
+
+// True if all zeros
+bool check_data(){
+  if (first) {
+    //Serial.println("First instruction");
+    first=false;
+    return true;
+  }
+  
+  //Serial.println("checking data");
+  //Serial.println(data[0], BIN);
+  // Serial.println(data_before[0], BIN);
+  int last = int(bitRead(data_before[0],7));
+ // Serial.println(last);
+  int now = int(bitRead(data[0],7));
+  //Serial.println(now);
+
+  if (last != now) {
+    //  Serial.println("different parity bits");
+      return true;
+  }
+  else{
+    //Serial.println("same parity bits");
+    return false; 
+  }
+ }
+/*
+// True if all zeros
+bool zero_data_before(){
+ // Serial.println("Checking data_before is zero");
+  byte b = 0;
+  for (int i=0; i<3; i++) {
+    if (!(data_before[i] == b)){
+   //   Serial.println("not zero");
+    return false;
+    }
+  }
+  //Serial.println("zero");
+  return true; 
+ }
+*/
