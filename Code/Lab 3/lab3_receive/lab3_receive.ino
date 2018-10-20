c2a5eeac8df1005057bb0714ef6170cfba4dc7c5
Ria Singh <rs2353@cornell.edu>
2018-10-20 18:38:15 -0400

    added positioning to receive code

--- Code/Lab 3/lab3_receive/lab3_receive.ino
+++ Code/Lab 3/lab3_receive/lab3_receive.ino
@@ -15,13 +15,14 @@ byte zero[3] = {0, 0, 0};
 byte data_before [3];
 byte data[3];
 int data_array[24];
-int count_x = -1;
-int count_y = 0;
+int x = 0;
+int y = 0;
 bool first = true;
+int facing;
 
 // Intialize strings to print
-String x = "0";
-String y = "1";
+String xstring;
+String ystring;
 String west;
 String east;
 String north;
@@ -32,85 +33,81 @@ String robot;
 void setup() {
   Serial.begin(9600);
   printf_begin();
-  //printf("\n\rBase Station Receive Code\n\r");
-  //printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
-  //
-  // Setup and configure rf radio
-  //
   radio.begin();
-  // optionally, increase the delay between retries & # of retries
   radio.setRetries(15,15);
   radio.setAutoAck(true);
-  // set the channel
   radio.setChannel(0x50);
-  // set the power
-  // RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_MED=-6dBM, and RF24_PA_HIGH=0dBm.
   radio.setPALevel(RF24_PA_MIN);
-  //RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
   radio.setDataRate(RF24_250KBPS);
-  // optionally, reduce the payload size.  seems to
-  // improve reliability
-  //radio.setPayloadSize(8);
-  //
-  // Open pipes to other nodes for communication
-  //
-  // This simple sketch opens two pipes for these two nodes to communicate
-  // back and forth.
-  // Open 'our' pipe for writing
-  // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-    radio.openWritingPipe(pipes[1]);
-    radio.openReadingPipe(1,pipes[0]);    //
-  // Start listening
-  //
-    radio.startListening();
-  //
-  // Dump the configuration of the rf unit for debugging
-  //
-  // radio.printDetails();
+  
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);    
+
+  radio.startListening();
+
+  radio.printDetails();
 }
 void loop() {
   // put your main code here, to run repeatedly:
-    if ( radio.available() )
-      {
-        // Dump the payloads until we've gotten everything
-        bool done = false;
-        while (!done)
-        {
-          // Fetch the payload, and see if this was the last one.
-          done=radio.read(&data,sizeof(data));
-          if (check_data()){
-             decipher();
-          }
-          copy(data, data_before, 3);
-          delay(20);
+    if ( radio.available() ) {
+      bool done = false;
+      while (!done) {
+        done=radio.read(&data,sizeof(data));
+        if (check_data()){
+           decipher();
         }
+        copy(data, data_before, 3);
+        delay(20);
       }
+    }
 }
 
-void decipher(){
-  //Serial.println("Deciphering");
+void decipher() {
+// Parse byte array into 1D int array
   for (int i = 0; i<3; i++){
     for (int j = 0; j < 8; j++){
-      int x = 8*i + j;
-      data_array[x] = int(bitRead(data_before[i], 7-j));
-      //Serial.println(x);
-     // Serial.print(data_array[x]);
+      int k = 8*i + j;
+      data_array[k] = int(bitRead(data_before[i], 7-j));
      }
    }
 
-   count_x++;
+ // Position Information
+  if (data[2] == 0) {
+     if (data[3] == 0) { facing = 0; }
+     else { facing = 1; }
+  }
+  else {
+    if (data[3] == 0) { facing = 2; }
+    else { facing = 3; } 
+  }
 
-   if (count_x > 2) {
-    count_y++;
-    count_x = 0;
-   }
-   if (count_y > 3) {
-    count_y = 0;
-   }
-   //Serial.println("");
+// North = 0; East = 1; South = 2; West = 3
+  switch(facing) {
+    case 0: x++;
+    case 1: y--;
+    case 2: x--;
+    case 3: y++;
+  }
+
+  //reset x and y values
+  if (x > 2 || x < 0) { x = 0; }
+  if (y > 3 || y < 0) { y = 0; }  
+
+ // Start at 0 and mark first false
+  if (first) {
+    x = 0;
+    y = 0;
+    first = false;
+  }
+  
+   xstring = String(x);
+   ystring = String(y);
+
+   // Other Robot Information
    if (data_array[1]==1) {robot = "true";}
    else {robot = "false";}  
-     
+
+   // Wall information
    if (data_array[4]==1) {north = "true";}
    else {north = "false";}
    
@@ -123,10 +120,7 @@ void decipher(){
    if (data_array[7]==1) {west = "true";}
    else {west = "false";}
 
-  x = String(count_x);
-  y = String(count_y);
-
-   Serial.println(x+","+y+","+"north="+north+","+"east="+east+","+"south="+south+","+"west="+west+","+"robot="+robot);
+   Serial.println(xstring+","+ystring+","+"north="+north+","+"east="+east+","+"south="+south+","+"west="+west+","+"robot="+robot);
  
 }
 
@@ -137,40 +131,17 @@ void copy(byte* src, byte* dst, int len) {
 // True if all zeros
 bool check_data(){
   if (first) {
-    //Serial.println("First instruction");
-    first=false;
     return true;
   }
-  
-  //Serial.println("checking data");
-  //Serial.println(data[0], BIN);
-  // Serial.println(data_before[0], BIN);
+
   int last = int(bitRead(data_before[0],7));
- // Serial.println(last);
   int now = int(bitRead(data[0],7));
-  //Serial.println(now);
 
   if (last != now) {
-    //  Serial.println("different parity bits");
       return true;
   }
   else{
-    //Serial.println("same parity bits");
     return false; 
   }
- }
-/*
-// True if all zeros
-bool zero_data_before(){
- // Serial.println("Checking data_before is zero");
-  byte b = 0;
-  for (int i=0; i<3; i++) {
-    if (!(data_before[i] == b)){
-   //   Serial.println("not zero");
-    return false;
-    }
-  }
-  //Serial.println("zero");
-  return true; 
- }
-*/
+}
+
