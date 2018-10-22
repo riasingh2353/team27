b45cbc887359b29f6c765627547e6ea61dc99fa9
Felicia Guo <31290768+FeliciaG@users.noreply.github.com>
2018-10-22 17:38:36 -0400

    Add files via upload

---
+++ Code/Sensor_Read.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+        Serial.println("loop");
+        int line_l = analogRead(A0);
+        int line_r = analogRead(A1);
+        int line_m = analogRead(A2);
+        int front_wall_value  = analogRead(A3);
+        int right_wall_value  = analogRead(A4);
+        Serial.print("Left Line: ");
+        Serial.println(line_l);
+        Serial.print("Right Line: ");
+        Serial.println(line_r);
+        Serial.print("Middle Line: ");
+        Serial.println(line_m);
+        Serial.print("Front: ");
+        Serial.println(front_wall_value);
+        Serial.print("Right: ");
+        Serial.println(right_wall_value);
+        delay(500);
+}
